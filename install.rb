1da975d0d47c5271cde4b473af66ecfb2aefddd5
John Kelly <johnke@gmail.com>
2017-09-11 09:18:29 +0200

    Initial commit

---
+++ install.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+
+dirname = File.expand_path(File.dirname(__FILE__))
+
+if File.exist?('~/.vimrc')
+  `mv ~/.vimrc ~/.vimrc.bak`
+end
+
+if File.exist?('~/.vim')
+  `mv ~/.vim ~/.vim.bak`
+end
+
+`ln -s #{dirname}/vimrc ~/.vimrc`
+`ln -s #{dirname}/dotvim ~/.vim`
